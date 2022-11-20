8eb4a11a04d0495feec455608aa0d5f83d70478c
ranga <rangaswamysake@gmail.com>
2022-11-20 11:52:09 +0530

    css chnages

--- wp-content/themes/astra/header.php
+++ wp-content/themes/astra/header.php
@@ -22,7 +22,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
 <?php wp_head(); ?>
 <?php astra_head_bottom(); ?>
 </head>
